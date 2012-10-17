7d77afe49d301c21f91c77ecbecfb2d57203ea53
Tim Cutts <tjrc@sanger.ac.uk>
2012-10-17 11:38:02 +0100

    Added documentation and link-type option
    Pod docs in the Config module, and implemented link-type configuration
    option, because I can't decide whether MemberOf or RefersTo is right, so
    let the user have the choice...

--- lib/RT/Action/LinkWasReplyTo.pm
+++ lib/RT/Action/LinkWasReplyTo.pm
@@ -13,6 +13,21 @@ sub Prepare {
 
 sub Commit {
     my $self            = shift;
+
+    my $r2r_config = RT->Config->Get('RepliesToResolved');
+
+    my $Ticket = $self->TicketObj;
+    my $queue = $Ticket->QueueObj->Name;
+
+    my $linktype = $r2r_config->{'default'}->{'link-type'};
+    if (exists($r2r_config->{$queue})) {
+        if (exists($r2r_config->{$queue}->{'link-type'})) {
+            $linktype = $r2r_config->{$queue}->{'link-type'};
+        }
+    }
+
+    return 1 unless (defined($linktype));
+
     my $Transaction     = $self->TransactionObj;
     my $FirstAttachment = $Transaction->Attachments->First;
     return 1 unless $FirstAttachment;
@@ -20,10 +35,11 @@ sub Commit {
     my $OldTicket = $FirstAttachment->GetHeader('X-RT-Was-Reply-To');
     return 1 unless $OldTicket;
 
-    my $Ticket = $self->TicketObj;
+    my ($val, $msg);
+    my $map = $Ticket->LINKTYPEMAP;
 
-    my ($val, $msg) = $Ticket->AddLink(Type => 'MemberOf',
-                                       Target => $OldTicket);
+    ($val, $msg) = $Ticket->AddLink(Type => $map->{$linktype}->{'Type'},
+                                    $map->{$linktype}->{'Mode'} => $OldTicket);
 
     if ($val == 0) {
         RT->Logger->error('Failed to link '.$Ticket->id.'to '.$OldTicket.": $msg\n");
